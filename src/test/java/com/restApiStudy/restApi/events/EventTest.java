8701602a376a4e95714162bb3f578d32afd3aaf1
sgjang11 <sgjang11@outlook.kr>
2023-05-11 17:57:51 +0900

    feat: 매개변수를 이용하여 중복 코드 제거

--- src/test/java/com/restApiStudy/restApi/events/EventTest.java
+++ src/test/java/com/restApiStudy/restApi/events/EventTest.java
@@ -1,10 +1,13 @@
 package com.restApiStudy.restApi.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
     @Test
     public void builder() {
@@ -32,67 +35,56 @@ public class EventTest {
     }
 
     @Test
-    public void testFree() {
+//    @Parameters({
+//            "0, 0, true",
+//            "100, 0, false",
+//            "0, 100, false"
+//    })
+    @Parameters(method = "parametersForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isFree()).isTrue();
-///////////////////////////////////////////////////////////
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
-///////////////////////////////////////////////////////////
-        // Given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
+    private Object[] parametersForTestFree() {
+        return new Object[]{
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
     }
 
     @Test
-    public void testOffline() {
+    @Parameters
+    public void testOffline(String location, boolean isOffline) {
         // Given
         Event event = Event.builder()
-                .location("강남역 네이버 D2 스타텁 팩토리")
+                .location(location)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue();
-///////////////////////////////////////////////////////////
-        // Given
-        event = Event.builder()
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isOffline()).isFalse();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
     }
 
+    private Object[] parametersForTestOffline() {
+        return new Object[]{
+                new Object[] {"강남", true},
+                new Object[] {null, false},
+                new Object[] {" ", false}
+        };
+    }
 
 }
